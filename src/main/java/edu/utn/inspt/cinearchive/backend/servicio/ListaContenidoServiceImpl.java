0e525e12db0c39b6855cd3288103fa8813f53254
FranDroidX <franco.vilaseco@hotmail.com>
2025-11-09 06:12:21 -0300

    Refactor: eliminar ListaContenidoService e impl redundante; consolidación en ListaService/Repository

--- src/main/java/edu/utn/inspt/cinearchive/backend/servicio/ListaContenidoServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package edu.utn.inspt.cinearchive.backend.servicio;
-
-// Stub deprecado: funcionalidad movida a ListaService/ListaRepository.
-// Mantener la clase para evitar referencias antiguas pero sin lógica ni dependencias.
-@Deprecated
-public class ListaContenidoServiceImpl { }
